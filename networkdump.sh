45ad44f8afe421fba20ad353c1bbcb2ff1d04c80
RobertgSchultz8 <71945176+RobertgSchultz8@users.noreply.github.com>
2020-10-17 13:23:18 -0400

    Add files via upload

---
+++ networkdump.sh
@@ -0,0 +1,14 @@
+#Creates our file if we don't have one
+#Also includes date script was run
+output=networkinfo"$(date +"%Y_%m_%d")".txt
+#Writes Ip address
+#Does not append to file
+#Essentially overwrites an old version of the file with a new one
+hostname -I | tee $output
+#Writes dns info
+#This does append to file
+cat /etc/resolv.conf | tee -a $output
+#Writes info on open ports
+#Info includes process that opened sockets
+#This too appends to file
+netstat -tulp | grep LISTEN | tee -a $output
